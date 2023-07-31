73925d5911a5941f667fff8c68f4186538d8de63
yspark2 <88123219+yspark2@users.noreply.github.com>
2023-07-31 14:59:10 +0900

    [level 0] Title: 최빈값 구하기, Time: 18.92 ms, Memory: 63.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun solution(array: IntArray): Int {
+        val input = array
+        val hashMap = HashMap<Int, Int>()
+        for(idx in 0 until input.size){
+            if(idx == 0 || !hashMap.containsKey(input[idx])){
+                hashMap[input[idx]] = 1
+            }else{
+                hashMap[input[idx]] = hashMap[input[idx]]!!.plus(1)
+            }
+        }
+        val sortedList = hashMap.toList().sortedByDescending { it.second }
+        if (sortedList.getOrNull(0)?.second == sortedList.getOrNull(1)?.second) {
+            return -1
+        } else {
+            val tempKey = sortedList.getOrNull(0)!!.first
+            return tempKey
+        }  
+    }
+}

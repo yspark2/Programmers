ea24aa6d4d3e0356b1b4632f25322c85582bfecf
yspark2 <88123219+yspark2@users.noreply.github.com>
2023-08-04 10:10:09 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.03 ms, Memory: 60 MB -BaekjoonHub

--- 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.kt
+++ 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.kt
@@ -1,18 +1,18 @@
 import kotlin.math.abs
 class Solution {
     fun solution(dots: Array<IntArray>): Int {
-        var answer: Int = 0
-        var x = 0
+        var x: Int = 0
+        var y = 0
         for (i in 0 until dots.size - 1) {
             for (j in i + 1..dots.size - 1) {
-                if (x == 0 && dots[i][0] == dots[j][0]) {
-                    answer = dots[i][0]
-                    x = abs(dots[i][1] - dots[j][1])
+                if (y == 0 && dots[i][0] == dots[j][0]) {
+                    x = dots[i][0]
+                    y = abs(dots[i][1] - dots[j][1])
                 } else if (dots[i][0] == dots[j][0]) {
-                    answer = abs(answer - dots[j][0])
+                    x = abs(x - dots[j][0])
                 }
             }
         }
-        return answer * x
+        return x * y
     }
 }

979e9b9a57d96dce070b9c9e50fa40d6f70addae
yspark2 <88123219+yspark2@users.noreply.github.com>
2023-08-08 00:23:00 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.02 ms, Memory: 61.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.kt
@@ -0,0 +1,30 @@
+
+class Solution {
+    fun solution(keyinput: Array<String>, board: IntArray): IntArray {        
+        var answer: IntArray = intArrayOf(0, 0)
+        for (i in keyinput.indices) {
+            when (keyinput[i]) {
+                "left" -> {
+                    if (-(board[0] / 2) < answer[0])
+                        answer[0]--
+                }
+
+                "right" -> {
+                    if (board[0] / 2 > answer[0])
+                        answer[0]++
+                }
+
+                "down" -> {
+                    if (-(board[1] / 2) < answer[1])
+                        answer[1]--
+                }
+
+                "up" -> {
+                    if (board[1] / 2 > answer[1])
+                        answer[1]++
+                }
+            }
+        }
+        return answer
+    }
+}

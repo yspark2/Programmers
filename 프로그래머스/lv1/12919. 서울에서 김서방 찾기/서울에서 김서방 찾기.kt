f317298939e11bbb43a84f6b680b7692c2494573
yspark2 <88123219+yspark2@users.noreply.github.com>
2023-09-15 11:05:59 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 11.45 ms, Memory: 60.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(seoul: Array<String>): String {
+        var answer = ""
+        for(i in seoul.indices){
+            if(seoul[i] == "Kim"){
+                answer =  "김서방은 ${i}에 있다"
+            }
+        }
+        return answer
+    }
+}

4f33c9fa7a62023f76c808a3dc16aeecac86b0f7
yspark2 <88123219+yspark2@users.noreply.github.com>
2023-09-29 23:42:12 +0900

    [level 1] Title: 음양 더하기, Time: 0.04 ms, Memory: 61.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun solution(absolutes: IntArray, signs: BooleanArray): Int {
+        var answer: Int = 0 
+        for(i in absolutes.indices){
+            if(signs[i] == true){
+                answer += absolutes[i]
+            }else{
+                answer -= absolutes[i]
+            }
+        }
+        return answer
+    }
+}
